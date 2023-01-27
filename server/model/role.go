4370b18c4d1e9acbaeb4b425552b8a203847d392
j-rockwell <john@llewkcor.com>
2023-01-26 18:51:57 -0800

    main: added 'omitempty' flag to role id field

--- server/model/role.go
+++ server/model/role.go
@@ -5,7 +5,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 const ROLE_COLL_NAME string = "roles"
 
 type Role struct {
-	ID          primitive.ObjectID `json:"id" bson:"_id"`
+	ID          primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	Name        string             `json:"name" bson:"name"`
 	DisplayName string             `json:"display_name" bson:"display_name"`
 	Permissions []Permission       `json:"permissions" bson:"permissions"`
