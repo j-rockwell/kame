ec263b2ea2535aca845071b39477614cbe070a4c
j-rockwell <john@llewkcor.com>
2023-01-26 18:52:23 -0800

    main: impl AuthWithCredentials# func

--- server/controller/auth.go
+++ server/controller/auth.go
@@ -1,12 +1,91 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"net/http"
+	"server/auth"
+	"server/config"
+	"server/database"
+	"server/model"
+)
 
 // AuthWithCredentials attempts to authenticate an account using email/password
 // combination and if successful will return a new auth token in 200 OK response
 func (controller *DataController) AuthWithCredentials() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
+	conf := config.Prepare()
+	accessTokenPubkey := conf.Auth.AccessTokenPubkey
+	accessTokenTTL := conf.Auth.AccessTokenTTL
+	refreshTokenPubkey := conf.Auth.RefreshTokenPubkey
+	refreshTokenTTL := conf.Auth.RefreshTokenTTL
+	isReleaseVersion := conf.Gin.Mode == "release"
 
+	mqp := database.MongoQueryParams{
+		MongoClient:    controller.Mongo,
+		DatabaseName:   controller.DatabaseName,
+		CollectionName: controller.CollectionName,
+	}
+
+	rqp := database.RedisQueryParams{
+		RedisClient: controller.Redis,
+	}
+
+	return func(ctx *gin.Context) {
+		var params model.AuthWithStandardCredentialsRequest
+
+		err := ctx.ShouldBindJSON(&params)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, GenerateErrorResponse("failed to bind params: "+err.Error()))
+			return
+		}
+
+		account, err := database.FindDocumentByKeyValue[string, model.Account](mqp, "email", params.Email)
+		if err != nil {
+			if err == mongo.ErrNoDocuments {
+				ctx.AbortWithStatus(http.StatusNotFound)
+				return
+			}
+
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, GenerateErrorResponse("failed to query account"))
+			return
+		}
+
+		if !auth.IsMatchedHash(params.Password, account.Password) {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, GenerateErrorResponse("password does not match"))
+			return
+		}
+
+		accessToken, err := auth.GenerateToken(account.ID.Hex(), accessTokenPubkey, int(accessTokenTTL))
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, GenerateErrorResponse("failed to generate access token"))
+			return
+		}
+
+		refreshToken, err := auth.GenerateToken(account.ID.Hex(), refreshTokenPubkey, int(refreshTokenTTL))
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, GenerateErrorResponse("failed to generate refresh token"))
+			return
+		}
+
+		res := model.AuthSuccessResponse{
+			AccessToken:  accessToken,
+			RefreshToken: refreshToken,
+		}
+
+		_, err = database.SetCacheValue(rqp, refreshToken, account.ID.Hex(), int(refreshTokenTTL))
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, GenerateErrorResponse("failed to cache refresh token"))
+			return
+		}
+
+		var cookieDomain = ".localhost"
+		if isReleaseVersion {
+			cookieDomain = "*.sushikame.com"
+		}
+
+		ctx.SetSameSite(http.SameSiteStrictMode)
+		ctx.SetCookie("refresh_token", refreshToken, int(refreshTokenTTL), "/", cookieDomain, true, true)
+		ctx.JSON(http.StatusOK, res)
 	}
 }
 
