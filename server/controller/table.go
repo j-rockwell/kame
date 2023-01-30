6a3b8db8d3770180a5a1209e022cee0eaf32fa37
j-rockwell <john@llewkcor.com>
2023-01-29 21:54:06 -0800

    main: added basic impl for CreateTable# func

--- server/controller/table.go
+++ server/controller/table.go
@@ -1,6 +1,15 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"net/http"
+	"reflect"
+	"server/database"
+	"server/model"
+	"time"
+)
 
 // GetTables will query and return all tables reserved
 // within the next 30 days.
@@ -28,8 +37,78 @@ func (controller *DataController) IsBlackoutDay() gin.HandlerFunc {
 
 // CreateTable will attempt to create a new table reservation
 func (controller *DataController) CreateTable() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
+	mqp := database.MongoQueryParams{
+		MongoClient:    controller.Mongo,
+		DatabaseName:   controller.DatabaseName,
+		CollectionName: controller.CollectionName,
+	}
 
+	return func(ctx *gin.Context) {
+		var params model.CreateTableRequest
+
+		err := ctx.ShouldBindJSON(&params)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, GenerateErrorResponse("failed to bind params"))
+			return
+		}
+
+		existingByAttendee, err := database.FindDocumentByKeyValue[primitive.ObjectID, model.Table](mqp, "attendee", params.Attendee)
+		if err != nil && err != mongo.ErrNoDocuments {
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, GenerateErrorResponse("failed to query existing table requests (attendee)"))
+			panic("failed to query existing table requests:\n" + err.Error())
+			return
+		}
+
+		existingByGroup, err := database.FindManyDocumentsByKeyValue[model.TableGroup, model.Table](mqp, "group", params.Group)
+		if err != nil && err != mongo.ErrNoDocuments {
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, GenerateErrorResponse("failed to query existing table requests (group)"))
+		}
+
+		if !reflect.ValueOf(existingByAttendee).IsNil() {
+			// TODO: Check if user has permissions to make more than one reservation for attendee
+			ctx.AbortWithStatusJSON(http.StatusConflict, GenerateErrorResponse("attendee has too many reservations"))
+			return
+		}
+
+		if !reflect.ValueOf(existingByGroup).IsNil() {
+			ctx.AbortWithStatusJSON(http.StatusConflict, GenerateErrorResponse("group is not available"))
+			return
+		}
+
+		if params.GroupSize < 1 {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, GenerateErrorResponse("group size must be greater than 1"))
+			return
+		}
+
+		if params.GroupSize > 10 {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, GenerateErrorResponse("group size exceeds 10"))
+			return
+		}
+
+		/*
+			TODO: Permission checks we need to perform:
+				- If req is creating a blackout, do they have perms to do so?
+		*/
+
+		table := model.Table{
+			CreatedBy:   params.CreatedBy,
+			Attendee:    params.Attendee,
+			CreatedAt:   time.Now(),
+			Group:       params.Group,
+			GroupSize:   params.GroupSize,
+			Time:        params.Time,
+			Transaction: params.Transaction,
+			Blackout:    params.Blackout,
+		}
+
+		result, err := database.InsertDocument[model.Table](mqp, table)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, GenerateErrorResponse("failed to insert document"))
+			panic("failed to insert document:\n" + err.Error())
+			return
+		}
+
+		ctx.JSON(http.StatusCreated, gin.H{"message": result})
 	}
 }
 
