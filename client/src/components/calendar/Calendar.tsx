05e25838f32b2e7307e4305a0059f8f1725c9431
j-rockwell <john@llewkcor.com>
2023-01-31 16:34:12 -0800

    main: calendar impl

--- client/src/components/calendar/Calendar.tsx
+++ client/src/components/calendar/Calendar.tsx
@@ -1,61 +1,38 @@
-import {useCallback, useMemo} from "react";
-import {getDayNamesFromCurrentDay, getDays, getMonths} from "@/data/Calendar";
-import {useScreenWidth} from "@/hooks/Width";
+import {useCallback, useEffect, useState} from "react";
+import {getCalendarData, getDayNamesFromCurrentDay, MonthEntry} from "@/data/Calendar";
 import {TableTime} from "@/models/Table";
-import {Box, SimpleGrid, Text} from "@chakra-ui/react";
+import {Box, Center, Select, SimpleGrid, Text, useColorModeValue} from "@chakra-ui/react";
 
 interface ICalendarProps {
   setTime: (t: TableTime) => void;
 }
 
-export const Calendar = ({time, setTime}: ICalendarProps) => {
-  const [calendarData, setCalendarData] = useState<MonthEntry[]>([]);
-  const [selectedMonth, setSelectedMonth] = useState<number>(time?.month ?? new Date().getMonth())
-
-  const WIDTH = useScreenWidth();
+export const Calendar = ({setTime}: ICalendarProps) => {
   const CURRENT_DATE = new Date();
   const START_MONTH: number = CURRENT_DATE.getMonth();
   const START_DAY: number = CURRENT_DATE.getDate();
   const START_YEAR: number = CURRENT_DATE.getFullYear();
+  const [calendarData, setCalendarData] = useState<MonthEntry[]>([]);
+  const [selectedDay, setSelectedDay] = useState<number>(START_DAY);
+  const [selectedMonth, setSelectedMonth] = useState<number>(START_MONTH);
+  const [selectedYear, setSelectedYear] = useState<number>(START_YEAR);
+
+  const selectedTextColor = useColorModeValue('text.light', 'text.dark');
+  const deselectedTextColor = useColorModeValue('textMuted.light', 'textMuted.dark');
 
   /**
-   * Returns true if this component is being viewed on a smaller screen
+   * Returns true if the provided month number matches the selected month
    */
-  const isSmallDevice = useMemo(() => {
-    return WIDTH <= 768;
-  }, [WIDTH]);
+  const isSelectedMonth = useCallback((month: number) => {
+    return month === selectedMonth;
+  }, [selectedMonth]);
 
   /**
-   * Returns an array of all the days in the
+   * Returns true if the provided day number matches the selected day
    */
-  const getDaysInMonth = useCallback(() => {
-    return getDays(
-      time?.day ?? START_DAY,
-      time?.month ?? START_MONTH,
-      time?.year ?? START_YEAR,
-      30
-    )
-  }, [START_DAY, START_MONTH, START_YEAR, time?.day, time?.month, time?.year]);
-
-  /**
-   * Returns an array of the next twelve months including the current month
-   */
-  const getMonthsInYear = useCallback(() => {
-    return getMonths(time?.month ?? START_MONTH, 11);
-  }, [START_MONTH, time?.month]);
-
-  /**
-   * Returns an array of day names to iterate over for calendar day display
-   */
-  const getDayNames = useCallback(() => {
-    const date = new Date(
-      time?.year ?? START_YEAR,
-      time?.month ?? START_MONTH,
-      time?.day ?? START_DAY
-    );
-
-    return getDayNamesFromCurrentDay(date.getDay());
-  }, [START_DAY, START_MONTH, START_YEAR, time?.day, time?.month, time?.year]);
+  const isSelectedDay = useCallback((month: number, day: number) => {
+    return (day === selectedDay && month === selectedMonth);
+  }, [selectedDay, selectedMonth]);
 
   /**
    * Returns a formatted name for the provided day of the week
@@ -64,20 +41,109 @@ export const Calendar = ({time, setTime}: ICalendarProps) => {
     return day.substring(0, 1).toUpperCase() + day.toLowerCase().substring(1, 3);
   }, []);
 
+  /**
+   * Returns a formatted month name
+   */
+  const getFormattedMonthName = useCallback((month: string) => {
+    return month.substring(0, 1).toUpperCase() + month.toLowerCase().substring(1);
+  }, []);
+
+  /**
+   * Returns calendar data for the given time values
+   */
+  const getData = useCallback(() => {
+    return getCalendarData(
+      START_DAY,
+      START_MONTH,
+      START_YEAR,
+      30
+    );
+  }, [START_DAY, START_MONTH, START_YEAR]);
+
+  /**
+   * Handles updating months when selected from the dropdown
+   */
+  const handleMonthChange = useCallback((index: string) => {
+    const n = Number(index);
+    const result = calendarData.find(m => m.index === n);
+
+    if (!result) {
+      console.error("failed to match monthIndex");
+      return;
+    }
+
+    setSelectedMonth(n);
+  }, [calendarData]);
+
+  /**
+   * Handles changing the global state for the selected table time
+   */
+  const handleTimeChange = useCallback((day: number, month: number, year: number) => {
+    if (
+      selectedDay === day
+      && selectedMonth === selectedMonth
+      && selectedYear === year
+    ) {
+      return;
+    }
+
+    if (selectedDay !== day) {
+      setSelectedDay(day);
+    }
+
+    if (selectedMonth != month) {
+      setSelectedMonth(month);
+    }
+
+    if (selectedYear != year) {
+      setSelectedYear(year);
+    }
+
+    setTime({day: day, month: month, year: year});
+  }, [selectedDay, selectedYear, selectedMonth, setTime]);
+
+  /**
+   * Initial data loading
+   */
+  useEffect(() => {
+    setCalendarData(getData());
+  }, [getData]);
+
   return (
     <Box>
-      <SimpleGrid columns={7} spacing={2}>
-        {getDayNames().map(name => (
+      <Center>
+        <Select w={'14rem'} onChange={e => handleMonthChange(e.target.value)}>
+          {calendarData.map(month => (
+            <option key={month.name} value={month.index}>{getFormattedMonthName(month.name)} {month.year}</option>
+          ))}
+        </Select>
+      </Center>
+
+      <SimpleGrid columns={7} spacing={2} mt={4}>
+        {getDayNamesFromCurrentDay(START_DAY).map(name => (
           <Box key={name} w={16} h={8}>
             <Text textAlign={'center'}>{getFormattedDayName(name)}</Text>
           </Box>
         ))}
+      </SimpleGrid>
 
-        {getDaysInMonth().map(day => (
-          <Box key={day} w={16} h={8}>
-            <Text textAlign={'center'}>{day}</Text>
+      <SimpleGrid columns={7} spacing={2}>
+        {calendarData.map(month => month.dates.map(date => (
+          <Box
+            key={date}
+            w={16}
+            h={8}
+            onClick={() => handleTimeChange(date, month.index, month.year)}
+            cursor={isSelectedMonth(month.index) ? 'pointer' : 'auto'}
+            bgColor={isSelectedDay(month.index, date) ? 'blue.500' : 'none'}>
+            <Text
+              textAlign={'center'}
+              color={isSelectedMonth(month.index) ? selectedTextColor : deselectedTextColor}
+            >
+              {date}
+            </Text>
           </Box>
-        ))}
+        )))}
       </SimpleGrid>
     </Box>
   );
