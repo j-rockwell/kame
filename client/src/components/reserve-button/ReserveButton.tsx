c28e3e61edd68e4d45f70d57b8d1859e7b7524b6
j-rockwell <john@llewkcor.com>
2023-02-09 12:29:51 -0800

    main: restyled reserve button to use nextlink

--- client/src/components/reserve-button/ReserveButton.tsx
+++ client/src/components/reserve-button/ReserveButton.tsx
@@ -1,34 +1,37 @@
-import {Box, Button, Text, useColorModeValue} from "@chakra-ui/react";
+import {default as NextLink} from 'next/link';
+import {Box, Link, Text, useColorModeValue} from "@chakra-ui/react";
 
 interface IReserveButtonProps {
   disclaimer?: string;
-  onClick: () => void;
   children: string;
 }
 
-export const ReserveButton = ({disclaimer, onClick, children}: IReserveButtonProps) => {
+export const ReserveButton = ({disclaimer, children}: IReserveButtonProps) => {
   const bgColor = useColorModeValue('info.light', 'info.dark');
   const mutedTextColor = useColorModeValue('textMuted.light', 'textMuted.dark');
 
   return (
     <Box w={'100%'}>
-      <Button
-        onClick={onClick}
-        size={'lg'}
-        fontSize={'xl'}
-        w={'100%'}
-        bgColor={bgColor}
-        borderRadius={12}>
-        {children}
-      </Button>
+      <NextLink href={'/create-reservation'} passHref>
+        <Link
+          as={'button'}
+          w={'100%'}
+          py={2}
+          fontSize={'xl'}
+          fontWeight={'bold'}
+          _hover={{textDecoration: 'none'}}
+          bgColor={bgColor}
+          borderRadius={12}>
+          {children}
+        </Link>
+      </NextLink>
 
       {disclaimer && (
         <Text
           textAlign={'center'}
           fontSize={'sm'}
           color={mutedTextColor}
-          mt={4}
-        >
+          mt={4}>
           {disclaimer}
         </Text>
       )}
