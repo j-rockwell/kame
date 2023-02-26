78366c94a8d9924c66a0d7ae28edd8e01412a2ed
j-rockwell <john@llewkcor.com>
2023-02-25 17:24:39 -0800

    main/server - feat/20: swapped TableMenu enum to use Menu docid

--- server/model/table.go
+++ server/model/table.go
@@ -1,7 +1,9 @@
 package model
 
 import (
+	"fmt"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"strings"
 	"time"
 )
 
@@ -11,10 +13,10 @@ type Table struct {
 	ID          primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	CreatedBy   primitive.ObjectID `json:"created_by" bson:"created_by"`
 	Attendee    primitive.ObjectID `json:"attendee" bson:"attendee"`
+	Menu        primitive.ObjectID `json:"menu" bson:"menu"`
 	CreatedAt   time.Time          `json:"created_at" bson:"created_at"`
 	Group       TableGroup         `json:"group" bson:"group"`
 	GroupSize   uint8              `json:"group_size" bson:"group_size"`
-	Menu        TableMenu          `json:"menu" bson:"menu"`
 	Time        TableTime          `json:"table_time" bson:"table_time"`
 	Transaction Transaction        `json:"transaction" bson:"transaction"`
 	Blackout    bool               `json:"blackout" bson:"blackout"`
@@ -27,13 +29,6 @@ const (
 	B TableGroup = "B"
 )
 
-type TableMenu string
-
-const (
-	PREMIUM   TableMenu = "PREMIUM"
-	SIGNATURE TableMenu = "SIGNATURE"
-)
-
 type GetTablesOnDateResponse struct {
 	Available []TableGroup `json:"available,omitempty"`
 }
@@ -42,9 +37,9 @@ type CreateTableRequest struct {
 	CreatedBy   primitive.ObjectID `json:"created_by" bson:"created_by"`
 	Attendee    primitive.ObjectID `json:"attendee" bson:"attendee"`
 	CreatedAt   time.Time          `json:"created_at" bson:"created_at"`
+	Menu        primitive.ObjectID `json:"menu" bson:"menu"`
 	Group       TableGroup         `json:"group" bson:"group"`
 	GroupSize   uint8              `json:"group_size" bson:"group_size"`
-	Menu        TableMenu          `json:"menu" bson:"menu"`
 	Time        TableTime          `json:"table_time" bson:"table_time"`
 	Transaction Transaction        `json:"transaction" bson:"transaction"`
 	Blackout    bool               `json:"blackout,omitempty" bson:"blackout,omitempty"`
@@ -52,10 +47,10 @@ type CreateTableRequest struct {
 
 type RescheduleTableRequest struct {
 	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Menu      primitive.ObjectID `json:"menu" bson:"menu"`
 	Group     TableGroup         `json:"group" bson:"group"`
 	GroupSize uint8              `json:"group_size" bson:"group_size"`
 	Time      TableTime          `json:"table_time" bson:"table_time"`
-	Menu      TableMenu          `json:"menu" bson:"menu"`
 }
 
 type CreateTableResponse struct {
@@ -64,4 +59,18 @@ type CreateTableResponse struct {
 
 type RescheduleTableResponse struct {
 	ID primitive.ObjectID `json:"table_id"`
+}
+
+// GetTableGroupFromString is a helper func to easily convert
+// strings to table groups
+func GetTableGroupFromString(s string) (TableGroup, error) {
+	if strings.ToLower(s) == "a" {
+		return A, nil
+	}
+
+	if strings.ToLower(s) == "b" {
+		return B, nil
+	}
+
+	return "", fmt.Errorf("invalid table group value")
 }
