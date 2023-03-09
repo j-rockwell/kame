65b7c948f0cafb5ab34c14e419323d9b4ce3ae19
j-rockwell <john@llewkcor.com>
2023-03-09 14:58:47 -0800

    main/client: wrapped app element in auth context provider

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import {ChakraProvider} from '@chakra-ui/react'
+import {AuthContextProvider} from "@/context/AuthContext.Provider";
 import {ReservationContextProvider} from "@/context/ReservationContext.Provider";
 import theme from '../styles/Theme';
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ReservationContextProvider>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </ReservationContextProvider>
+    <AuthContextProvider>
+      <ReservationContextProvider>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </ReservationContextProvider>
+    </AuthContextProvider>
   )
 }
