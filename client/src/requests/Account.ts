0a0a89f97f8d525a1f31a5b2eec3bd386f1fb877
j-rockwell <john@llewkcor.com>
2023-03-09 14:25:17 -0800

    main/client: fixed bad request body for createAccount

--- client/src/requests/Account.ts
+++ client/src/requests/Account.ts
@@ -13,7 +13,14 @@ export async function createAccount(
 ): Promise<CreateAccountResponse> {
   return new Promise<CreateAccountResponse>(async (resolve, reject) => {
     try {
-      const res = await axios.post<CreateAccountResponse>(`${API_URL}/account/`, {req});
+      const res = await axios.post<CreateAccountResponse>(`${API_URL}/account/`, {
+        "first_name": req.first_name,
+        "last_name": req.last_name,
+        "email_address": req.email_address,
+        "password": req.password,
+        "phone": req.phone,
+        "preferences": req.preferences,
+      });
 
       if (!res || !res.data) {
         return reject(new Error('query response empty'));
