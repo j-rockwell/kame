0092e734c1b0af39b44d309ce9910f77c2f43aab
j-rockwell <john@llewkcor.com>
2023-10-13 14:46:54 -0700

    dev/server: permission camelcase fix

--- server/model/permission.go
+++ server/model/permission.go
@@ -3,25 +3,25 @@ package model
 type Permission string
 
 const (
-	CREATE_TABLE      Permission = "create_table"      // Create
-	MODIFY_TABLE      Permission = "modify_table"      // Modify or delete tables
-	CREATE_BLACKOUT   Permission = "create_blackout"   // Able to create new blackout dates
-	MODIFY_BLACKOUT   Permission = "modify_blackout"   // Able to modify existing blackout dates
-	GRANT_ROLES       Permission = "grant_roles"       // Able to grant entire roles to other accounts
-	GRANT_PERMISSIONS Permission = "grant_permissions" // Able to grant specific permissions to other accounts
-	VIEW_ROLES        Permission = "view_roles"        // Able to see all permissions and users who have them assigned
-	VIEW_TABLES       Permission = "view_tables"       // Able to see tables for other users
+	CreateTable      Permission = "create_table"      // Create
+	ModifyTable      Permission = "modify_table"      // Modify or delete tables
+	CreateBlackout   Permission = "create_blackout"   // Able to create new blackout dates
+	ModifyBlackout   Permission = "modify_blackout"   // Able to modify existing blackout dates
+	GrantRoles       Permission = "grant_roles"       // Able to grant entire roles to other accounts
+	GrantPermissions Permission = "grant_permissions" // Able to grant specific permissions to other accounts
+	ViewRoles        Permission = "view_roles"        // Able to see all permissions and users who have them assigned
+	ViewTables       Permission = "view_tables"       // Able to see tables for other users
 )
 
 func GetAllPermissions() []Permission {
 	return []Permission{
-		CREATE_TABLE,
-		MODIFY_TABLE,
-		CREATE_BLACKOUT,
-		MODIFY_BLACKOUT,
-		GRANT_PERMISSIONS,
-		GRANT_ROLES,
-		VIEW_ROLES,
-		VIEW_TABLES,
+		CreateTable,
+		ModifyTable,
+		CreateBlackout,
+		ModifyBlackout,
+		GrantPermissions,
+		GrantRoles,
+		ViewRoles,
+		ViewTables,
 	}
 }
